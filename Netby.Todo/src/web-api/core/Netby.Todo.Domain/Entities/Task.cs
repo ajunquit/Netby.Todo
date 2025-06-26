8e4c9a440d36b6bcfb6ddff8ee2633eaccb5d763
Alejandro Junqui <ajunquit@gmail.com>
2025-06-25 23:07:00 -0500

    update Task

--- Netby.Todo/src/web-api/core/Netby.Todo.Domain/Entities/Task.cs
+++ Netby.Todo/src/web-api/core/Netby.Todo.Domain/Entities/Task.cs
@@ -6,7 +6,7 @@
         public Guid ID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime CreationDate { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
         public DateTime ExpirationDate { get; set; }
         public StatusTask Status { get; set; }
     }
