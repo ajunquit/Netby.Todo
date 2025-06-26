814c9084a7a4d2919ae0fe9d257244b37297bc0c
Alejandro Junqui <ajunquit@gmail.com>
2025-06-26 07:43:34 -0500

    add application module function

--- Netby.Todo/src/web-api/application/Netby.Todo.Application/ApplicationModule.cs
+++ Netby.Todo/src/web-api/application/Netby.Todo.Application/ApplicationModule.cs
@@ -1,8 +1,15 @@
 ﻿namespace Netby.Todo.Application
 {
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
     public static class ApplicationModule
     {
 
-
+        public static IServiceCollection AddApplicationModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+            return services;
+        }
     }
 }
