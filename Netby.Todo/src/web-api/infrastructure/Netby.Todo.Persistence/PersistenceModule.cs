c9a4bdd5ecf78f434a83cee8e4c5eaa0222ab995
Alejandro Junqui <ajunquit@gmail.com>
2025-06-25 23:22:50 -0500

    add connection string and PersistenceModule

--- Netby.Todo/src/web-api/infrastructure/Netby.Todo.Persistence/PersistenceModule.cs
+++ Netby.Todo/src/web-api/infrastructure/Netby.Todo.Persistence/PersistenceModule.cs
@@ -1,6 +1,19 @@
-﻿namespace Netby.Todo.Persistence
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Netby.Todo.Persistence.Contexts;
+
+namespace Netby.Todo.Persistence
 {
-    public class PersistenceModule
+    public static class PersistenceModule
     {
+        public static IServiceCollection AddPersistenceModule(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<NetbyDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("Local"))
+                );
+            return services;
+        }
     }
 }
--- Netby.Todo/src/web-api/presentation/Netby.Todo.API/Program.cs
+++ Netby.Todo/src/web-api/presentation/Netby.Todo.API/Program.cs
@@ -1,3 +1,4 @@
+using Netby.Todo.Persistence; 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddPersistenceModule(builder.Configuration);
 
 var app = builder.Build();
 
