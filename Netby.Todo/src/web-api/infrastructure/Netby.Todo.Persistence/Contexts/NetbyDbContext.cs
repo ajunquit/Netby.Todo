62d6d7a067ec73a99412bb933edf2b957ded9ebc
Alejandro Junqui <ajunquit@gmail.com>
2025-06-25 23:00:29 -0500

    add NetbyDbContext

---
+++ Netby.Todo/src/web-api/infrastructure/Netby.Todo.Persistence/Contexts/NetbyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Netby.Todo.Persistence.Contexts
+{
+    public class NetbyDbContext: DbContext
+    {
+        public DbSet<Task> Users { get; set; }
+        public NetbyDbContext(DbContextOptions<NetbyDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
