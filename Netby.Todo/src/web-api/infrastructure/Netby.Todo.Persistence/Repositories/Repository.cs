6a74451d5c4fd8457a0ac024c4b6b26479fa9edf
Alejandro Junqui <ajunquit@gmail.com>
2025-06-26 08:02:52 -0500

    add repositories

---
+++ Netby.Todo/src/web-api/infrastructure/Netby.Todo.Persistence/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿namespace Netby.Todo.Persistence.Repositories
+{
+    using Microsoft.EntityFrameworkCore;
+    using Netby.Todo.Application.Services.Repositories;
+    using Netby.Todo.Persistence.Contexts;
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly NetbyDbContext _netbyDbContext;
+        protected readonly DbSet<T> _entities;
+
+        public Repository(NetbyDbContext netbyDbContext)
+        {
+            _netbyDbContext = netbyDbContext;
+            _entities = _netbyDbContext.Set<T>();
+        }
+        public int Count()
+        {
+            return _entities.Count();
+        }
+
+        public bool Delete(string id)
+        {
+            T entity = Get(id);
+            if (entity != null)
+            {
+                _entities.Remove(entity);
+                return true;
+            }
+            return false;
+        }
+
+        public T Get(string id)
+        {
+            return _entities.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.ToList();
+        }
+
+        public bool Insert(T entity)
+        {
+            _entities.Add(entity);
+            return true;
+        }
+
+        public bool Update(T entity)
+        {
+            _entities.Update(entity);
+            return true;
+        }
+    }
+}
---
+++ Netby.Todo/src/web-api/infrastructure/Netby.Todo.Persistence/Repositories/TaskRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Netby.Todo.Persistence.Repositories
+{
+    using Netby.Todo.Application.Services.Repositories;
+    using Netby.Todo.Domain.Entities;
+    using Netby.Todo.Persistence.Contexts;
+    public class TaskRepository : Repository<TaskItem>, ITaskRepository
+    {
+        public TaskRepository(NetbyDbContext netbyDbContext): base(netbyDbContext)
+        {
+        }
+    }
+}
