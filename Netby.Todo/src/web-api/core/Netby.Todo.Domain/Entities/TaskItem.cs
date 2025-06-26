c32c5301e670cc79013c125fa003a2d670133b29
Alejandro Junqui <ajunquit@gmail.com>
2025-06-26 07:07:02 -0500

    rename entity task

---
+++ Netby.Todo/src/web-api/core/Netby.Todo.Domain/Entities/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿namespace Netby.Todo.Domain.Entities
+{
+    using Netby.Todo.Domain.Enums;
+
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime ExpirationDate { get; set; }
+        public StatusTask Status { get; set; } = StatusTask.Pending;
+    }
+}
